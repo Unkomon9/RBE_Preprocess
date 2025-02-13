56627a077ce7d8ad668005950483e410502be269
Sam <chensam730@gmail.com>
2025-02-12 18:07:21 -0600

    init skeleton of the script

--- something.py
+++ something.py
@@ -24,6 +24,11 @@
 #         - ir_tokens = preprocess(ir_tokens, func_details)
 #         - insert_rule(c_source, ir_tokens, rbe_file, rule_num)
 
+
+import subprocess
+import sys
+
+# need to change, doesn't look right 
 def create_new_rule(rule_num:int) -> None: 
     with open("rbe_file.txt", "r") as f:
         lines = f.readlines()
@@ -31,7 +36,30 @@ def create_new_rule(rule_num:int) -> None:
             lines.append("\n")
     with open("rbe_file.txt", "w") as f:
         f.writelines(lines)
+
+def insert_rule(c_source:str, ir_tokens:list[str], rbe_file, rule_num:list) -> None: 
+    if rule_num not in range(1, 100): 
+        print("Rule number does not exist")
+        return 
+    create_new_rule(rule_num)
     
 
+def compile(c_source:str) -> list[str]: 
+    try: 
+        subprocess.run(["gcc", c_source])
+        return ["ir", "tokens"]
+    
+    except Exception as e: 
+        print(f"Failed to compile C source: {e}")
+        sys.exit(1)
+    
+    
+def preprocess(ir_tokens:list[str], function_details) -> ir_tokens:
+    # do some stuff that needs to be further looked at
+    return ir_tokens
 
+def main(c_source:str, function_details, rbe_file, rule_num):
+    ir_tokens = compile(c_source)
+    ir_tokens = preprocess(ir_tokens, function_details)
+    insert_rule(c_source, ir_tokens, rbe_file, rule_num)
 
